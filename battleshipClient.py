3e2dcf0a7ee897ca11952bbb78f0f942c6db7997
Dillon Monday <dillon.g@bresnan.net>
2018-09-18 16:03:27 -0600

    messages send

--- battleshipClient.py
+++ battleshipClient.py
@@ -1,10 +1,16 @@
 import http.client
+import sys
 
 HOST_NAME = '10.152.191.254'
 PORT_NUMBER = '8080'
+xcoord = sys.argv[1]
+ycoord = sys.argv[2]
 
 connection = http.client.HTTPConnection("localhost:"+PORT_NUMBER)
-connection.request("GET","/")
+connection.request("GET","/","x=" +xcoord + "&y="+ ycoord)
 response = connection.getresponse()
-print("status is " +response.status)
+result = response.read()
+
+print("status is " +(str(response.status)))
+print("the result is " + str(result))
 print("finished")

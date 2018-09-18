aa34a8ab1855a9a340d02013ecf6f8e27c02ed23
Jase Rost <jaserost@yahoo.com>
2018-09-18 14:50:45 -0600

    Added POST functionality

--- battleshipClient.py
+++ battleshipClient.py
@@ -1,10 +1,13 @@
 import http.client
+import sys
 
-HOST_NAME = '10.152.191.254'
+HOST_NAME = '0.0.0.0'
 PORT_NUMBER = '8080'
+xcoord = sys.argv[1]
+ycoord = sys.argv[2]
 
 connection = http.client.HTTPConnection("localhost:"+PORT_NUMBER)
-connection.request("GET","/")
+connection.request("POST","/","x=" + xcoord + "&y=" + ycoord)
 response = connection.getresponse()
-print("status is " +response.status)
+print("The response is " + (str)(response.status))
 print("finished")
